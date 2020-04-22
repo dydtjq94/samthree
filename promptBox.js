cd92efd714924ab67ef5d10dc96095076e18bd82
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-04-22 18:23:46 +0900

    Update promptBox.js
    왼쪽 오른쪽 버튼 업데이트

--- promptBox.js
+++ promptBox.js
@@ -2,7 +2,45 @@ const prompt = document.querySelector(`.prompt`);
 const date = document.querySelector(`.date`);
 const leftButton = document.querySelector(`.left`);
 const rightButton = document.querySelector(`.right`);
-console.log(prompt);
-console.log(date);
-console.log(leftButton);
-console.log(rightButton);
+const firstInput = document.querySelector(`.first__prompt`);
+const secondInput = document.querySelector(`.second__prompt`);
+const thirdInput = document.querySelector(`.third__prompt`);
+
+console.log(firstInput, secondInput, thirdInput);
+
+let title = [`밥버거`, `고추장`, `아이유`, `손흥민`, `하이루`, `키키키`];
+let num = 0;
+prompt.innerHTML = title[num];
+
+function handleLeftClick(event) {
+  num = num - 1;
+  if (num < 0) {
+    num = 5;
+  } else {
+  }
+  prompt.innerHTML = title[num];
+
+  firstInput.innerHTML = `${prompt.innerHTML.split("")[0]}:`;
+  secondInput.innerHTML = `${prompt.innerHTML.split("")[1]}:`;
+  thirdInput.innerHTML = `${prompt.innerHTML.split("")[2]}:`;
+}
+
+function handleRightClick(event) {
+  num = num + 1;
+  if (num > 5) {
+    num = 0;
+  } else {
+  }
+  prompt.innerHTML = title[num];
+
+  firstInput.innerHTML = `${prompt.innerHTML.split("")[0]}:`;
+  secondInput.innerHTML = `${prompt.innerHTML.split("")[1]}:`;
+  thirdInput.innerHTML = `${prompt.innerHTML.split("")[2]}:`;
+}
+
+function init() {
+  leftButton.addEventListener("click", handleLeftClick);
+  rightButton.addEventListener("click", handleRightClick);
+}
+
+init();
