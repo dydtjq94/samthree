cb1e13cba518e17a726745742b6c7a282ceb9d02
dydtjq94 <62597465+dydtjq94@users.noreply.github.com>
2020-04-22 17:17:24 +0900

    correct

---
+++ promptBox.js
@@ -0,0 +1,8 @@
+const prompt = document.querySelector(`.prompt`);
+const date = document.querySelector(`.date`);
+const leftButton = document.querySelector(`.left`);
+const rightButton = document.querySelector(`.right`);
+console.log(prompt);
+console.log(date);
+console.log(leftButton);
+console.log(rightButton);
